5bec31b8d2cb9262240a8a079c50f92ebf598116
zyra capili <zcapili@cambridge.org>
2018-03-02 13:08:41 +0800

    add new block for Live

--- manifests/site.pp
+++ manifests/site.pp
@@ -8,7 +8,11 @@ node 'ip-10-111-33-11' {
         include '::ntp'
 		include ::role::educ::tng
 	}
-
+#node '<LIVE HOSTNAME>' {
+#       include '::ntp'
+#		include ::role::educ::tng
+#	}
+	
 hiera_include('classes',[])
 node default {
   hiera_resources('resources',{})
